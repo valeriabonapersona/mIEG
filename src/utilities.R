d9cafcfb2cd338cf5feca3d6ee86a0f25756b45b
Valeria Bonapersona <valeria.bonapersona@gmail.com>
2020-07-27 16:10:49 +0200

    set seeds

--- src/utilities.R
+++ src/utilities.R
@@ -9,6 +9,8 @@
 # Environment preparation -------------------------------------------------
 rm(list = ls())
 
+set.seed(992)
+
 source("src/dependencies.R")
 source("src/general_functions.R")
 source("config/paths.R")
