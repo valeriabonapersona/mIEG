5cc015394ddaaaf2c3703d850c6f8dfc5e574573
Valeria Bonapersona <valeria.bonapersona@gmail.com>
2020-07-27 10:10:47 +0200

    init

---
+++ src/utilities.R
@@ -0,0 +1,12 @@
+## Script: "Utilities"
+
+## project: mIEG
+
+## Author: Valeria Bonapersona
+## contact: v.bonapersona-2 (at) umcutrecht.nl
+
+
+# Environment preparation -------------------------------------------------
+rm(list = ls())
+
+source("src/dependencies.R")
