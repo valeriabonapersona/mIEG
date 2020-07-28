419ca9a8694745ddbc60564098241250d28f880d
Valeria Bonapersona <valeria.bonapersona@gmail.com>
2020-07-28 12:25:27 +0200

    init

---
+++ src/metaanalysis.R
@@ -0,0 +1,15 @@
+## Script: "Meta-analysis cFos"
+
+## project: mIEG
+
+## Input: preprocessed data from data_preparation.R adjusted by report_descriptives.rmd
+## Output: meta-analytic results
+
+## Author: Valeria Bonapersona & Heike Schuler
+## contact: v.bonapersona-2 (at) umcutrecht.nl
+
+
+
+# Environment preparation -------------------------------------------------
+source("src/utilities.R")
+df <- 
