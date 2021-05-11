317f38a0d20a2146537d6242e3b336d454895880
Valeria Bonapersona <valeria.bonapersona@gmail.com>
2021-05-11 14:12:25 +0200

    visualizations main analyses

--- src/metaanalysis.R
+++ src/metaanalysis.R
@@ -24,7 +24,17 @@ df_filtered <- df %>% filter(sex == "M")
 #Check for influential outliers and remove.
 res <- rma(yi, vi, measure="ZCOR", data=df_filtered)
 inf <- influence(res)
-df_filtered <- df_filtered[-which(inf$inf$inf == '*'),]
+df_filtered <- df_filtered[-which(inf$inf$inf == '*'),] %>%
+  mutate(
+    tAcuteStressor = ifelse(tStressorType == "NA", 0, 1), 
+    stressor_intensity = case_when(
+      tStressorType %in% c('OFT','DLB','NE','EPM') ~ "mild", # @ heike --> shouldnt this be mild? >> below you had 1
+      tStressorType %in% c('CRD','FST','MWM','RS',
+                           'FS in inhibitory avoidance task',
+                           'Shock in shock-probe burial task') ~"severe", 
+      TRUE ~ "mild"
+    )
+  )
 
 
 # Model -------------------------------------------------------------------
@@ -32,7 +42,9 @@ mod <- rma.mv(yi, vi,
               random = list(~1 | each, ~1 | nest),
               method = "REML",
               data = df_filtered,
-              mods = ~ tAcuteStressor:areaLevel2 -1, 
+              
+              mods = ~ areaLevel2:factor(tAcuteStressor):hit2 -1, 
+           #   mods = ~ tAcuteStressor:areaLevel2 -1, 
               slab = paste(authors, year, sep=", "))
 
 
@@ -41,18 +53,19 @@ mod <- rma.mv(yi, vi,
 interactions <- names(data.frame(mod$X))
 
 # RQ1: effects of ELA on presence acute stressor
-acutestressor <- ifelse(str_detect(interactions, "tAcuteStressor1"), 1, 0)
+acutestressor <- ifelse(str_detect(interactions, "tAcuteStressor.1"), 1, 0)
 contr_acutestressor <- rbind(
   ifelse(acutestressor == 1, 1/sum(acutestressor == 1), 0), # acute vs 0
   ifelse(acutestressor == 0, 1/sum(acutestressor == 0), 0) # baseline vs 0
 )
 
 anova(mod, L = contr_acutestressor) 
-summary(glht(mod, linfct = contr_acutestressor, df=df.residual(mod)), test=adjusted('holm'))
+main_res <- summary(glht(mod, linfct = contr_acutestressor, df=df.residual(mod)), test=adjusted('holm'))
 
 
 # RQ2: effects of ELA on brain area
-brainarea <- sub(".*2", "", interactions)
+brainarea <- gsub('(.*)\\.\\w+', '\\1', interactions)
+
 contr_brainarea <- NULL
 for (area in unique(brainarea)) {
   x <- ifelse(brainarea == area, 1/sum(brainarea == area), 0)
@@ -60,8 +73,7 @@ for (area in unique(brainarea)) {
 }
 
 anova(mod, L = contr_brainarea) 
-summary(glht(mod, linfct = contr_brainarea, df=df.residual(mod)), test = adjusted('holm'))
-
+res_ba <- summary(glht(mod, linfct = contr_brainarea, df=df.residual(mod)), test = adjusted('holm'))
 
 # Subgroup: hit2 ----------------------------------------------------------
 # estimates for each subgroup
@@ -69,14 +81,14 @@ subgroup_hit0 <- rma.mv(yi, vi,
                         random = list(~1 | each, ~1 | nest),
                         method = "REML",
                         data = df_filtered,
-                        mods = ~ tAcuteStressor:areaLevel2 -1,
+                        mods = ~ factor(tAcuteStressor):areaLevel2 -1,
                         subset = (hit2==0),
                         slab = paste(authors, year, sep=", "))
 subgroup_hit1 <- rma.mv(yi, vi, 
                         random = list(~1 | each, ~1 | nest),
                         method = "REML",
                         data = df_filtered,
-                        mods = ~ tAcuteStressor:areaLevel2 -1,
+                        mods = ~ factor(tAcuteStressor):areaLevel2 -1,
                         subset = (hit2==1),
                         slab = paste(authors, year, sep=", "))
 
@@ -85,7 +97,7 @@ comparison_hit <- data.frame(estimate = c(coef(subgroup_hit0), coef(subgroup_hit
                              stderror = c(subgroup_hit0$se, subgroup_hit1$se),
                              meta = c(rep("hit0", length(subgroup_hit0$se)),
                                       rep("hit1", length(subgroup_hit1$se))), 
-                             stressor = c('BL','AS','BL','AS','BL','AS','BL','BL',
+                             stressor = c('BL','AS','BL','AS','BL','AS','AS','AS',
                                           'BL','AS','BL','AS','BL','AS','BL','AS','BL','AS'),
                              tau2 = c(rep(subgroup_hit0$tau2, length(subgroup_hit0$se)),
                                       rep(subgroup_hit1$tau2, length(subgroup_hit1$se))))
@@ -97,7 +109,130 @@ rma(estimate, sei=stderror, mods = ~ meta -1, method="FE", data=comparison_hit, 
 # with(comparison_hit, round(c(zval = (estimate[1] - estimate[2])/sqrt(stderror[1]^2 + stderror[2]^2)), 3))
 
 # meta-analyze interaction 2nd hit by stressor
-rma(estimate, sei=stderror, mods = ~ meta:stressor -1, method="FE", data=comparison_hit, digits=3)
+hit_mod <- rma(estimate, sei=stderror, mods = ~ meta:stressor -1, method="FE", data=comparison_hit, digits=3)
+
+
+
+# Exploratory: severity ----------------------------------------------------------
+df_filtered %>% 
+  filter(tAcuteStressor == "1") %>%
+  group_by(hit2, stressor_intensity) %>% 
+  count()
+
+severity_res <- rma.mv(yi, vi, 
+                  random = list(~1 | each, ~1 | nest),
+                  method = "REML",
+                  data = df_filtered,
+                  mods = ~stressor_intensity:hit2 -1,
+                  subset = (tAcuteStressor=="1"),
+                  slab = paste(authors, year, sep=", "))
+
+# Visualizations ----------------------------------------------------------
+## baseline & acute stress
+df_fig_main <- data.frame(
+  type = c("Acute stress", "Baseline"), 
+  facet = rep("Main analysis", 2),
+  g = main_res$test$coefficients, 
+  sem = main_res$test$sigma,
+  pval = main_res$test$pvalues, 
+  n = df_filtered %>% group_by(tAcuteStressor) %>% count() %>% arrange(-tAcuteStressor) %>% pull(n)
+)
+
+g_main <- df_fig_main %>%
+  mutate(type = factor(type, levels = c("Baseline", "Acute stress"))) %>%
+  ggplot(aes(type, g)) + 
+  geom_bar(stat = "identity", fill = "white", colour = "black") + 
+  geom_errorbar(aes(ymin = g-sem, ymax = g+sem), width = 0.2) +
+  
+  # beautiful
+  ylim(c(-0.2,1.1)) + 
+  theme_bw() + 
+  labs(x="", y = "Hedge's g") + 
+  facet_grid(~facet) + 
+  geom_text(x = "Baseline", y = max(df_fig_main$g) + 0.4, label = "*", size = 20) + 
+  geom_text(aes(y = 0.05, label = paste("n = ", n), size = 20)) + 
+  theme(text = element_text(size = 20), 
+        axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5), 
+        legend.position = "none")
+
+## second hit
+df_fig_hit <- data.frame(
+  hit = c("no hit", "2nd hit", "no hit", "2nd hit"),
+  type = c("Acute stress", "Acute stress", "Baseline", "Baseline"), 
+  g = hit_mod$b, 
+  sem = hit_mod$se,
+  pval = hit_mod$pval,
+  n = df_filtered %>% group_by(tAcuteStressor, hit2) %>% count() %>% arrange(-tAcuteStressor) %>% pull(n)
+)
+
+g_hit <- df_fig_hit %>%
+  mutate(type = factor(type, levels = c("Baseline", "Acute stress"))) %>%
+  mutate(hit = factor(hit, levels = c("no hit", "2nd hit"))) %>%
+    mutate(text = ifelse(pval < 0.05, "*", "")) %>%
+    
+  ggplot(aes(hit, g)) + 
+  geom_bar(stat = "identity", fill = "white", colour = "black") + 
+  geom_errorbar(aes(ymin = g-sem, ymax = g+sem), width = 0.2) +
+  facet_grid(~type) + 
+  
+  # beautiful
+  ylim(c(-0.3,1.3)) + 
+  theme_bw() + 
+  labs(x="", y = "Hedge's g") + 
+  geom_text(aes(label = text, y = g+0.3),  size = 20) + 
+  geom_text(aes(y = 0.05, label = paste("n = ", n), size = 20)) + 
+  theme(text = element_text(size = 20), 
+        axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5),
+        legend.position = "none")
+
+
+ggarrange(
+  g_main, g_hit, nrow = 1, widths = c(1,2),
+  labels = c("A)", "B)")
+)
+
+## brain area
+df_fig_ba <- data.frame(
+  ba = c("AMY", "HPF", "HYP", "AMY", "HPF", "HYP", "PFC", "TH", "PFC", "TH"),
+  type = c("Baseline", "Baseline", "Baseline", "Acute stress", "Acute stress", 
+           "Acute stress", "Acute stress","Acute stress", "Baseline", "Baseline"), 
+  g = res_ba$test$coefficients, 
+  sem = res_ba$test$sigma,
+  pval = res_ba$test$pvalues
+) %>% 
+  left_join(
+    df_filtered %>% 
+      group_by(tAcuteStressor, areaLevel2) %>% 
+      count() %>% 
+      rename(ba = areaLevel2, type = tAcuteStressor) %>% 
+      mutate(type = ifelse(type == 1, "Acute stress", "Baseline"))
+  )
+
+g_ba <- df_fig_ba %>%
+  mutate(
+    type = factor(type, levels = c("Baseline", "Acute stress")),
+    ba = case_when(
+      ba == "AMY" ~ "Amygdala", 
+      ba == "PFC" ~ "Prefrontal cx", 
+      ba == "HYP" ~ "Hypothalamus", 
+      ba == "HPF" ~ "Hippocampus", 
+      ba == "TH" ~ "Thalamus"
+    )) %>%
+  ggplot(aes(ba, g)) + 
+  geom_bar(stat = "identity", fill = "white", colour = "black") + 
+  geom_errorbar(aes(ymin = g-sem, ymax = g+sem), width = 0.2) +
+  facet_grid(~type) + 
+  
+  # beautiful
+  ylim(c(-0.5,1.1)) + 
+  theme_bw() + 
+  labs(x="", y = "Hedge's g") + 
+  geom_text(aes(y = -0.5, label = paste("n = ", n), size = 20)) + 
+  theme(text = element_text(size = 20),
+        axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5),
+        legend.position = "none")
+
+g_ba
 
 # Sensitivity: Species ----------------------------------------------------
 # model compared to complete model
@@ -141,73 +276,7 @@ comparison_sens <- data.frame(estimate = c(coef(mod), coef(mod_sens)),
                                        rep(mod_sens$tau2, length(mod_sens$se))))
 
 # meta-analyze with fixed effect
-res <- rma(estimate, sei=stderror, mods = ~ sens -1, method="FE", data=comparison_sens, digits=3)
-
-
-# Exploratory: Stressor Type ----------------------------------------------
-#categorize into groups. 
-phys <- c('CRD','FS in inhibitory avoidance task','Shock in shock-probe burial task','RS')
-psyc <- c('EPM','FST','NE','OFT')
-
-df_filtered$tStressorTypeGroup[df_filtered$tStressorType %in% phys] <- 'physical'
-df_filtered$tStressorTypeGroup[df_filtered$tStressorType %in% psyc] <- 'psychological'
-df_filtered$tStressorTypeGroup[df_filtered$tStressorType == 'NA'] <- 'none'
-
-# estimates for each subgroup
-subgroup_phys <- rma.mv(yi, vi, 
-                        random = list(~1 | each, ~1 | nest),
-                        method = "REML",
-                        data = df_filtered,
-                        mods = ~ areaLevel2 -1,
-                        subset = (tStressorTypeGroup=='physical'),
-                        slab = paste(authors, year, sep=", "))
-subgroup_psyc <- rma.mv(yi, vi, 
-                        random = list(~1 | each, ~1 | nest),
-                        method = "REML",
-                        data = df_filtered,
-                        mods = ~ areaLevel2 -1,
-                        subset = (tStressorTypeGroup=='psychological'),
-                        slab = paste(authors, year, sep=", "))
-
-# summarize together
-comparison_type <- data.frame(estimate = c(coef(subgroup_phys), coef(subgroup_psyc)), 
-                              stderror = c(subgroup_phys$se, subgroup_psyc$se),
-                              meta = c(rep("physical", length(subgroup_phys$se)),
-                                       rep("psychological", length(subgroup_psyc$se))), 
-                              tau2 = c(rep(subgroup_phys$tau2, length(subgroup_phys$se)),
-                                       rep(subgroup_psyc$tau2, length(subgroup_psyc$se))))
-
-# meta-analyze with fixed effect
-rma(estimate, sei=stderror, mods = ~ meta -1, method="FE", data=comparison_type, digits=3)
-
-
-# Exploratory: Novelty ----------------------------------------------------
-# estimates for each subgroup
-subgroup_novel <- rma.mv(yi, vi, 
-                         random = list(~1 | each, ~1 | nest),
-                         method = "REML",
-                         data = df_filtered,
-                         mods = ~ areaLevel2 -1,
-                         subset = (tNovel == 1),
-                         slab = paste(authors, year, sep=", "))
-subgroup_old <- rma.mv(yi, vi, 
-                       random = list(~1 | each, ~1 | nest),
-                       method = "REML",
-                       data = df_filtered,
-                       mods = ~ areaLevel2 -1,
-                       subset = (tNovel == 0),
-                       slab = paste(authors, year, sep=", "))
-
-# summarize together
-comparison_novelty <- data.frame(estimate = c(coef(subgroup_old), coef(subgroup_novel)), 
-                                 stderror = c(subgroup_old$se, subgroup_novel$se),
-                                 meta = c(rep("old", length(subgroup_old$se)),
-                                          rep("novel", length(subgroup_novel$se))), 
-                                 tau2 = c(rep(subgroup_old$tau2, length(subgroup_old$se)),
-                                          rep(subgroup_novel$tau2, length(subgroup_novel$se))))
-
-# meta-analyze with fixed effect
-rma(estimate, sei=stderror, mods = ~ meta -1, method="FE", data=comparison_novelty, digits=3)
+rma(estimate, sei=stderror, mods = ~ sens -1, method="FE", data=comparison_sens, digits=3)
 
 
 # Exploratory: Time -------------------------------------------------------
@@ -219,36 +288,6 @@ metaregression_time <- rma.mv(yi, vi,
                               subset = (tAcuteStressor == 1 & outMeasure == 'M'),
                               slab = paste(authors, year, sep=", "))
 metaregression_time
-
-### OR ###
-
-# estimates for each subgroup
-subgroup_early <- rma.mv(yi, vi, 
-                         random = list(~1 | each, ~1 | nest),
-                         method = "REML",
-                         data = df_filtered,
-                         mods = ~ areaLevel2 -1,
-                         subset = (tAcuteStressor == 1 & outMeasure == 'P'& tWaitPeriod <= 60),
-                         slab = paste(authors, year, sep=", "))
-subgroup_late <- rma.mv(yi, vi, 
-                        random = list(~1 | each, ~1 | nest),
-                        method = "REML",
-                        data = df_filtered,
-                        mods = ~ areaLevel2 -1,
-                        subset = (tAcuteStressor == 1 & outMeasure == 'P' & tWaitPeriod > 60),
-                        slab = paste(authors, year, sep=", "))
-
-# summarize together
-comparison_time <- data.frame(estimate = c(coef(subgroup_early), coef(subgroup_late)), 
-                              stderror = c(subgroup_early$se, subgroup_late$se),
-                              meta = c(rep("early", length(subgroup_early$se)),
-                                       rep("late", length(subgroup_late$se))), 
-                              tau2 = c(rep(subgroup_early$tau2, length(subgroup_early$se)),
-                                       rep(subgroup_late$tau2, length(subgroup_late$se))))
-
-# meta-analyze with fixed effect
-rma(estimate, sei=stderror, mods = ~ meta -1, method="FE", data=comparison_time, digits=3)
-
 
 # Exploratory: Out Measure ------------------------------------------------
 # estimates for each subgroup
@@ -280,3 +319,19 @@ rma(estimate, sei=stderror, mods = ~ meta -1, method="FE", data=comparison_measu
 
 
 ### END ###
+
+# Publication bias --------------------------------------------------------
+funnel(mod, xlab = "Hedge's g")
+mod_uni <- rma(yi, vi, 
+              method = "REML",
+              data = df_filtered,
+              mods = ~ tAcuteStressor:areaLevel2 -1, 
+              slab = paste(authors, year, sep=", "))
+regtest(mod_uni)
+
+high_es <- c(26452320, 24513388)
+
+
+# Forest plot -------------------------------------------------------------
+
+forest(mod)
